0e2e5a6e7f2ee29e0294ce27468f606377fc61fb
helloboge <65157937+helloboge@users.noreply.github.com>
2025-03-25 20:36:37 +0800

    Update Informer.py

--- models/Informer.py
+++ models/Informer.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from utils.masking import TriangularCausalMask, ProbMask
+from formers.utils.masking import TriangularCausalMask, ProbMask
 from formers.layers.Transformer_EncDec import Decoder, DecoderLayer, Encoder, EncoderLayer, ConvLayer
 from formers.layers.SelfAttention_Family import FullAttention, ProbAttention, AttentionLayer
 from formers.layers.Embed import DataEmbedding
